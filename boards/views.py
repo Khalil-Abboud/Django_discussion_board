9291467522ee0ea8a5a54dffb91c0647aa9cc0ee
Khalil-Abboud <khalil.ad.1998.98.com>
2025-12-14 18:47:33 +0300

    protacting views

--- boards/views.py
+++ boards/views.py
@@ -4,31 +4,36 @@ from .models import Board
 from django.contrib.auth.models import User
 from .models import Topic,Post
 from .forms import NewTopicForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     boards = Board.objects.all()
     return render(request,'home.html',{'boards':boards})
+
 def about (request):
     return HttpResponse(request)
+
 def board_topics(request,board_id):
     #board = Board.objects.get(pk=board_id)
     board = get_object_or_404(Board,pk=board_id)
     return render(request,'topics.html',{'board':board})
+
+@login_required
 def new_topic(request,board_id):
     board = get_object_or_404(Board,pk=board_id)
-    user = User.objects.first()
+    #user = User.objects.first()
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.created_by = user
+            topic.created_by = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
-                created_by=user,
+                created_by=request.user,
                 topic=topic
             )
             return redirect('board_topics',board_id=board_id)
@@ -50,3 +55,8 @@ def new_topic(request,board_id):
     #     )
     #     return redirect('board_topics',board_id=board.pk)
     return render(request,'new_topic.html',{'board': board,'form':form})
+
+def topic_posts (request,board_id,topic_id):
+    topic = get_object_or_404(Topic,board__pk=board_id,pk=topic_id)
+
+    return render(request,'topic_posts.html',{'topic':topic})

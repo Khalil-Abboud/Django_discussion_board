b6648b01ebd5904525b1edeeeaf9766165cfc25f
Khalil-Abboud <khalil.ad.1998.98.com>
2026-01-05 08:06:38 +0300

    Fix pagination and topic view handling

--- boards/views.py
+++ boards/views.py
@@ -5,12 +5,21 @@ from django.contrib.auth.models import User
 from .models import Topic,Post
 from .forms import NewTopicForm,PostForm
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Count
+from django.views.generic import UpdateView,ListView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
 # Create your views here.
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request,'home.html',{'boards':boards})
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request,'home.html',{'boards':boards})
+
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
 def about (request):
     return HttpResponse(request)
@@ -18,7 +27,16 @@ def about (request):
 def board_topics(request,board_id):
     #board = Board.objects.get(pk=board_id)
     board = get_object_or_404(Board,pk=board_id)
-    return render(request,'topics.html',{'board':board})
+    queryset =  board.topics.order_by('-date').annotate(comments=Count('posts'))
+    page = request.GET.get('page',1)
+    paginator = Paginator(queryset,20)
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        topics = paginator.page(1)
+    except EmptyPage:
+        topics = paginator.page(paginator.num_pages)
+    return render(request,'topics.html',{'board':board,'topics':topics})
 
 @login_required
 def new_topic(request,board_id):
@@ -58,9 +76,13 @@ def new_topic(request,board_id):
 
 def topic_posts (request,board_id,topic_id):
     topic = get_object_or_404(Topic,board__pk=board_id,pk=topic_id)
-
+    session_key = f'view_topic_{topic.pk}'
+    if not request.session.get(session_key,False):
+        topic.views+=1
+        topic.save()
+        request.session[session_key] = True
     return render(request,'topic_posts.html',{'topic':topic})
-
+    
 @login_required
 def reply_topic(request,board_id,topic_id):
     topic = get_object_or_404(Topic,board__pk=board_id,pk=topic_id)
@@ -71,7 +93,25 @@ def reply_topic(request,board_id,topic_id):
             post.topic = topic
             post.created_by = request.user
             post.save()
+            topic.updated_by = request.user
+            topic.updated_date = timezone.now()
+            topic.save()
             return redirect('topic_posts',board_id=board_id,topic_id=topic_id)
     else:
         form = PostForm()
     return render(request,'reply_topic.html',{'topic':topic,'form':form})
+
+@method_decorator(login_required,name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+    
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_date = timezone.now()
+        post.save()
+        return redirect('topic_posts',board_id = post.topic.board.pk, topic_id = post.topic.pk)
